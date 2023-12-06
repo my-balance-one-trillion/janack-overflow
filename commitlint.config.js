6286717c13a7d7d2b6f02bb21f89b81604198862
JasonYesBro <spdlqjqlwjs@naver.com>
2023-12-06 15:02:03 +0900

    #5 testtest11

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,3 @@
 module.exports = { 
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']]
-    }
+    extends: ['@commitlint/config-conventional']
 };
