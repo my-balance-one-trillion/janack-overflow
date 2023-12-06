258ca96a88c283a67b6554dc5d4208384900784d
JasonYesBro <spdlqjqlwjs@naver.com>
2023-12-06 14:23:06 +0900

    chore: issue

--- addIssueNumber.sh
+++ addIssueNumber.sh
@@ -1,9 +1,18 @@
 COMMIT_MSG_FILE=$1
+ISSUE_NUMBER=$(get_issue_number) # 이슈 번호를 얻는 로직을 작성해야 합니다.
 
-branch_name=`git rev-parse --abbrev-ref HEAD`
-issue_number=`echo ${branch_name} | cut -d '#' -f2`
-first_line=`head -n1 ${COMMIT_MSG_FILE}`
+COMMIT_MSG=$(cat $COMMIT_MSG_FILE)
 
-if [ -z "$first_line" ]; then
-    sed -i ".bak" "1s/^/[#$issue_number] /" ${COMMIT_MSG_FILE}
-fi
+echo "#${ISSUE_NUMBER} ${COMMIT_MSG}" > $COMMIT_MSG_FILE
+
+get_issue_number() {
+  # 이슈 번호를 얻는 로직을 작성합니다.
+  # 예시로는 git branch 이름에서 이슈 번호를 추출하는 방법을 사용할 수 있습니다.
+  BRANCH_NAME=$(get_git_branch_name)
+  ISSUE_NUMBER=$(expr "$BRANCH_NAME" : '.*#\([0-9]*\)')
+  echo $ISSUE_NUMBER
+}
+
+get_git_branch_name() {
+  git symbolic-ref --short HEAD
+}
