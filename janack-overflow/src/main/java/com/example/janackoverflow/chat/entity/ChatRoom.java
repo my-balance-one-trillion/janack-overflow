aa4f96c0349a02a5ee0c927ac4d9a21eba140abe
sonsbe <sbj9873@naver.com>
2023-12-11 14:06:37 +0900

    feat: 메인 페이지 기능 구현

--- janack-overflow/src/main/java/com/example/janackoverflow/chat/entity/ChatRoom.java
+++ janack-overflow/src/main/java/com/example/janackoverflow/chat/entity/ChatRoom.java
@@ -1,22 +1,35 @@
 package com.example.janackoverflow.chat.entity;
 
 import com.example.janackoverflow.chat.config.WebSocketConfig;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
+@Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoom {
-
-    private Long id;
-
+    private String roomId;
     private String roomName;
+    private long userCount;
 
-    @Builder
-    public ChatRoom(String roomName){
-        this.roomName = roomName;
+    private HashMap<String, String> userList = new HashMap<String, String>();
+
+    public ChatRoom create(String roomName){
+        ChatRoom chatRoom = ChatRoom.builder()
+                .roomId(UUID.randomUUID().toString())
+                .roomName(roomName)
+        .build();
+
+        return chatRoom;
     }
 }
--- janack-overflow/src/main/java/com/example/janackoverflow/mypage/controller/MypageController.java
+++ janack-overflow/src/main/java/com/example/janackoverflow/mypage/controller/MypageController.java
@@ -47,7 +47,7 @@ public class MypageController {
 //    내가쓴 댓글 보기
     @GetMapping("/mypage/mycomment/{usersid}")
     public ResponseEntity readMyComment(@PathVariable("usersid") Long usersId){
-        Page<CommentDTO.CommentResponseDto> myCommentList = mypageService.readMyComment(usersId);
+        Page<CommentDTO.ResponseDto> myCommentList = mypageService.readMyComment(usersId);
         return new ResponseEntity(myCommentList ,HttpStatus.OK);
     }
 
