c8fb698c30b61520eb5d586157a479b17d244390
Yoon sangHeyok <119560458+vaem6877@users.noreply.github.com>
2023-12-11 16:57:23 +0900

    chore: #27 package 준비

--- client/src/pages/Mypage.vue
+++ client/src/pages/Mypage.vue
@@ -1,7 +1,9 @@
 <template>
   <main>
     <aside>
-      <div class="sidemenu-wrap rounded-2xl border-1">sss</div>
+      <div class="sidemenu-wrap rounded-2xl border-1 text-lg text-cyan-200">
+        sss
+      </div>
     </aside>
     <section></section>
   </main>
