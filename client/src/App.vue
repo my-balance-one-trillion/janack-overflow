515a794bebdadf3d52f473b20bceaec96a477cda
JasonYesBro <spdlqjqlwjs@naver.com>
2023-12-13 09:07:20 +0900

    feat: #28 디펜던시 설정

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,22 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import PrismEditor from "@/components/PrismEditor.vue"
 </script>
 
 <template>
-  <div class="bg-blue-200 page-wrapper">
-    <Header></Header>
-    <Router-View />
-    <div class="test">
-      text test 한글 테스트 <i class="fa-regular fa-circle-user"> </i>
+    <div class="overflow-hidden h-max page-wrapper">
+        <Header></Header>
+        <Router-View />
     </div>
-  </div>
 </template>
 
 <style scoped>
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css";
 
 .test {
-  width: 100px;
-  height: 100px;
-  background-color: blue;
-  margin: 0 auto;
+    width: 100px;
+    height: 100px;
+    background-color: blue;
+    margin: 0 auto;
 }
 </style>
