c5666adde426645853302d507e71bd26d9a27ac2
JasonYesBro <spdlqjqlwjs@naver.com>
2023-12-14 18:46:59 +0900

    chore: #28 @vaem6877 님의 pinia code로 수정

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="container mx-auto max-w-custom">
-    <Header></Header>
-    <Router-View />
-  </div>
+    <div class="container mx-auto max-w-custom">
+        <Header></Header>
+        <Router-View v-if="userInfoLoaded" />
+    </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import { onMounted } from "vue";
+import { onBeforeMount, ref } from "vue";
 import Header from "@/components/Header.vue";
-import { useAuthStore } from "./stores/auth";
+import { useAuthStore } from "@/stores/auth";
 
+const authStore = useAuthStore();
+const userInfoLoaded = ref(false);
 
-onMounted(async () => {
-  if (useAuthStore().token) {
-    await getUserInfo();
-  }
+onBeforeMount(async () => {
+    if (authStore.token) {
+        await getUserInfo();
+        console.log("사용자 정보가 스토어에 등록되었습니다.")
+        userInfoLoaded.value = true;
+    }
 });
-
 async function getUserInfo() {
-  const response = await axios.get("/mypage/myinfo", {
-    headers: {
-      authorization: useAuthStore().token,
-    },
-  });
-  useAuthStore().setUserInfo(response.data);
-  console.log(useAuthStore().userInfo)
+    const response = await axios.get("/mypage/myinfo", {
+        headers: {
+            authorization: authStore.token,
+        },
+    });
+    authStore.setUserInfo(response.data);
+    console.log("함수 내 유저 인포 생성.");
 }
-console.log(useAuthStore().userInfo)
+console.log("함수 외 유저 인포 생성.")
 </script>
 
 <style scoped>
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css";
+
 .max-w-custom {
-  max-width: 1300px;
+    max-width: 1300px;
 }
 </style>
