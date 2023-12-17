d8daa68132acc550e75d5d5977b4f06b2908eb8a
JasonYesBro <spdlqjqlwjs@naver.com>
2023-12-17 14:12:16 +0900

    feat: #28 Footer 색상 연하게, Nav bar margin 수정

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-	<footer class="bg-gray-300 flex justify-between items-center p-9">
-		<div class="text-gray-700 font-light">
+	<footer class="flex items-center justify-between bg-gray-100 p-9">
+		<div class="font-light text-gray-700">
 			<p>개인정보처리방침 | 이용약관</p>
 			<p>(주) 내잔액1조 | 대표자: 전승현 | 사업자번호: 123-45-678910</p>
 			<p>주소 : 서울시 송파구 중대로 135, IT벤처타워 서관 12층</p>
