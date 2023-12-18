c7fa340d4327924ed871f8f60d395be28fb680c9
gossong <thdwldh99@gmail.com>
2023-12-18 14:13:50 +0900

    feat: #34 월별 적금 내역, 적금 횟수 구현

--- janack-overflow/src/main/java/com/example/janackoverflow/issue/repository/SolutionRepository.java
+++ janack-overflow/src/main/java/com/example/janackoverflow/issue/repository/SolutionRepository.java
@@ -1,18 +1,17 @@
 package com.example.janackoverflow.issue.repository;
 
-import com.example.janackoverflow.issue.domain.SolutionDTO;
-import com.example.janackoverflow.issue.domain.response.SolutionResponseDTO;
 import com.example.janackoverflow.issue.entity.Solution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import java.util.List;
 
 @Repository
 public interface SolutionRepository extends JpaRepository<Solution, Long> {
-    List<Solution> findAllByIssueIdOrderByCreatedAtDesc(Long issueId);
+    List<Solution> findAllByIssueIdOrderByCreatedAtDesc(Long issue_id);
     Optional<Solution> findByIssue_Id(long issueId);
+
+    List<Solution> findByIssueId(Long id);
 }
