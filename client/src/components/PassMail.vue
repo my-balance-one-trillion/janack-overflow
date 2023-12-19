05bf1588f329ac72a813281d52520b499b8dc30e
undercover0072 <flotter586@gmail.com>
2023-12-19 14:06:12 +0900

    feat: #31 회원가입 로그인 css 수정

--- client/src/components/PassMail.vue
+++ client/src/components/PassMail.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen bg-white py-6 flex flex-col justify-center sm:py-12">
     <div class="relative py-3 sm:max-w-xl sm:mx-auto">
       <div
-        class="absolute inset-0 bg-gradient-to-r from-red-300 to-red-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+        class="absolute inset-0 bg-gradient-to-r from-green-700 to-red-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
       </div>
 
       <div class="relative px-4 py-10 bg-gray-50 shadow-lg sm:rounded-3xl sm:p-20">
