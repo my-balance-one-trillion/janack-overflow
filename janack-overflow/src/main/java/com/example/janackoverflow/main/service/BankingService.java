ee1004e7534ee6ac65750959f0c5e97bbc2f4130
sonsbe <sbj9873@naver.com>
2023-12-28 11:05:06 +0900

    feat: #29: 최종 수정

--- janack-overflow/src/main/java/com/example/janackoverflow/main/service/BankingService.java
+++ janack-overflow/src/main/java/com/example/janackoverflow/main/service/BankingService.java
@@ -7,19 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.*;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import javax.print.DocFlavor;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 @Slf4j
 @Service
@@ -40,46 +39,65 @@ public class BankingService {
     @Value("${external.api.fin-acno}")
     private String finAcno;
 
-//    계좌 생성
+    //    계좌 생성
     public String createAccount(Long id) throws JsonProcessingException {
-        return ""+id+3020000009687L;
+        return "" + id + 3020000009687L;
     }
+
     //계좌 이체
     //에러 해결 시 적금
     public void transfer(int amount, String acntNum) throws JsonProcessingException {
 
-            String apiNm = "ReceivedTransferAccountNumber";
-            List<String> timeList = createTime();
-            ApiHeader apiHeader = new ApiHeader(iscd, accessToken, apiNm, timeList.get(0), timeList.get(1), createIsTuno());
 
-            ObjectMapper mapper = new ObjectMapper();
-            String headerJson = mapper.writeValueAsString(apiHeader);
+        String apiNm = "ReceivedTransferAccountNumber";
+        List<String> timeList = createTime();
+        ApiHeader apiHeader = new ApiHeader(iscd, accessToken, apiNm, timeList.get(0), timeList.get(1), createIsTuno());
 
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-            JSONObject request = new JSONObject()
-                    .put("Header", new JSONObject(headerJson))
-                    .put("Acno", acntNum)
-                    .put("Bncd", "011")
-                    .put("Tram", Integer.toString(amount))
-                    .put("DractOtlt", "출금테스트")
-                    .put("MractOtlt", "입금테스트");
-            HttpEntity<String> httpEntity = new HttpEntity<>(request.toString(), httpHeaders);
-            System.out.println("httpEntity:" + httpEntity);
-            ResponseEntity<String> response = restTemplate.exchange(baseURL + apiNm + ".nh", HttpMethod.POST, httpEntity, String.class);
-            JSONObject jsonObject = new JSONObject(response.getBody());
-            System.out.println(jsonObject.toString());
+        ObjectMapper mapper = new ObjectMapper();
+        String headerJson = mapper.writeValueAsString(apiHeader);
 
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject request = new JSONObject()
+                .put("Header", new JSONObject(headerJson))
+                .put("Acno", acntNum)
+                .put("Bncd", "011")
+                .put("Tram", Integer.toString(amount))
+                .put("DractOtlt", "출금테스트")
+                .put("MractOtlt", "입금테스트");
+
+        //WebClient
+        WebClient webClient = WebClient.builder()
+                .baseUrl(baseURL)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        String res = webClient.post()
+                .uri(apiNm + ".nh")
+                .body(BodyInserters.fromValue(request.toString()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println("응답:"+res);
+
+
+//        HttpEntity<String> httpEntity = new HttpEntity<>(request.toString(), httpHeaders);
+//        System.out.println("httpEntity:" + httpEntity);
+//        ResponseEntity<String> response = restTemplate.exchange(baseURL + apiNm + ".nh", HttpMethod.POST, httpEntity, String.class);
+//        JSONObject jsonObject = new JSONObject(response.getBody());
+//        System.out.println(jsonObject.toString());
 
     }
 
 
-//    거래 고유번호 생성
-    public String createIsTuno(){
+    //    거래 고유번호 생성
+    public String createIsTuno() {
         return Long.toString(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
     }
+
     //거래 시간 생성
-    public List<String> createTime(){
+    public List<String> createTime() {
         LocalDateTime localDateTime = LocalDateTime.now();
 
         List<String> timeList = new ArrayList<>();
